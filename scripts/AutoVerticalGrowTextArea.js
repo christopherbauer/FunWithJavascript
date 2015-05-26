5e657c50bd726e86ac5e95a993c95e6d710cc03f
Christopher Bauer <chrisbauer.career@gmail.com>
2015-05-26 10:08:17 -0400

    Added Expand Focused Textareas documents.
    My naming is all over the place and I apologize. The textareas
    vertically exand as long as the table row they are in has focus.

---
+++ scripts/AutoVerticalGrowTextArea.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+	$('table tr').on('focus', 'input,textarea', function(event) {
+		var $blurFrom = $(event.delegateTarget);
+		$blurFrom.css("background-color","yellow");
+		
+		$blurFrom.find("textarea[cols]").each(function(i, element) {
+			expandHeight($(element));
+		});
+	});
+	$('table tr').on('focusout', 'input,textarea', function(event) {
+		var $blurFrom = $(event.delegateTarget);
+		$blurFrom.css("background-color","white");
+		
+		$blurFrom.find("textarea[cols]").each(function(i, element) {
+			collapseHeight($(element));
+		});
+	});
+	 
+	function expandHeight($this){
+		var rows = ($this.val().length / ($this.prop("cols")-2) + 1);
+		$this.prop("rows",rows);
+	}
+	function collapseHeight($this){
+		$this.prop("rows", 1);
+	}
+});
