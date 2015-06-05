b2f5bbacbf19e0588d7d32db5b976aff1e615f91
Christopher Bauer <chrisbauer.career@gmail.com>
2015-06-05 12:43:05 -0400

    Fix to modularize tapscrollgallery
    Yay

--- scripts/TapScrollGallery.js
+++ scripts/TapScrollGallery.js
@@ -1,50 +1,67 @@
 (function() {
 	"use strict";
-
-
-	var pageSize = 3;
-	var page = 0;
-	var itemCount = 0;
-
-	//**********************DELME
-	for(var i = 0; i < 20; i++) {
-		$("#fixed").append("<div data-rank=\""+i+"\">"+(i+1)+"</div>");
-	}
-	//**********************DELME
-
-	refreshItemCount();
-
-	$("#next").on("click",function() {
-		if((page+1)*pageSize < itemCount){
-			page++;
+	console.log("Loaded TapGallery");
+	
+	var TapGallery = function(options) {
+	var defaults = { 
+			PageSize: 3, 
+			Page: 0, 
+			Selectors : { 
+				Gallery: null, 
+				Next :  null, 
+				Previous: null
+			} 
+		};
+		
+		options = $.extend(defaults, options);
+		
+		var itemCount;
+		
+		function rankElements() {
+			var $elements = $(options.Selectors.Gallery).find("*");
+			$elements.each(function (i, element) {
+				$(element).attr("data-rank",i);
+				
+			});
 		}
 		
-		scrollTo();
-	});
-	$("#previous").on("click",function() {
-		if(page > 0) {
-			page--;
+		rankElements();
+		
+		function refreshItemCount() {
+			itemCount = $(options.Selectors.Gallery).find("[data-rank]").length;
 		}
 		
-		scrollTo();
-	});
-
-	function refreshItemCount() {
-		itemCount = $("#fixed div[data-rank]").length;
-	}
-
-	function scrollTo() {
-		var $scrollElement = $($("#fixed div[data-rank='"+[page * pageSize]+"'"));
+		refreshItemCount();	
 		
-		var layout = ($scrollElement.outerWidth(true)-$scrollElement.innerWidth()) / 2;
+		$(options.Selectors.Next).on("click",function() {
+			if((options.Page+1)*options.PageSize < itemCount){
+				options.Page++;
+			}
 			
-		var pageAdjustment = page > 0 ? layout : 0;
+			scrollTo();
+		});
 		
-		var $parent = $scrollElement.parent();
-		
-		var elementScrollLeft = ($scrollElement.position().left - layout) + $parent.scrollLeft() + pageAdjustment - $parent.offset().left;
-		
-		$("#fixed").stop().animate({ scrollLeft: elementScrollLeft }, 1000);
-	}
-
+		$(options.Selectors.Previous).on("click",function() {
+			if(options.Page > 0) {
+				options.Page--;
+			}
+			
+			scrollTo();
+		});
+		function scrollTo() {
+			var $scrollElement = $(options.Selectors.Gallery).find("[data-rank='"+[options.Page * options.PageSize]+"']");
+			console.log($scrollElement);
+			var layout = ($scrollElement.outerWidth(true)-$scrollElement.innerWidth()) / 2;
+				
+			var pageAdjustment = options.Page > 0 ? layout : 0;
+			
+			var $parent = $scrollElement.parent();
+			
+			var elementScrollLeft = ($scrollElement.position().left - layout) + $parent.scrollLeft() + pageAdjustment - $parent.offset().left;
+			
+			$(options.Selectors.Gallery).stop().animate({ scrollLeft: elementScrollLeft }, 1000);
+		}
+	};
+	
+	window.TapGallery = TapGallery;
 })();
