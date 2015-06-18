ab850dacb2913cfc401224dc5e8fcc0703401139
Christopher Bauer <chrisbauer.career@gmail.com>
2015-06-18 15:48:53 -0400

    Fix for touch vs mouse events
    Strategy pattern for handling mouse vs touch.

--- scripts/SwipeDetector.js
+++ scripts/SwipeDetector.js
@@ -15,31 +15,49 @@
 		
 		var config = $.extend(defaults,options, { Start: null });
 		
-		var clear = function () {
+		var handlers = {};
+		
+		var clear = function (type) {
 			config.Start = null;
-			$(config.Selector).off("mousemove", mouseMove);
+			$(config.Selector).off(type, handlers[type]);
 		};
-
-		var mouseMove = function (data) {
+		var handleMove = function(x, type) {
 			if (!config.Start) {
-				config.Start = data.pageX;
+				config.Start = x;
 			}
 			
-			var dx = data.pageX - config.Start;
+			var dx = x - config.Start;
+			
 			if (dx > config.Threshold) {
-				clear();
-				$(this).trigger("swipe-right");
+				clear(type);
+				$(config.Selector).trigger("swipe-right");
 			} else if (dx < -config.Threshold) {
-				clear();
-				$(this).trigger("swipe-left");
+				clear(type);
+				$(config.Selector).trigger("swipe-left");
 			}
 		};
+		
+		handlers["mousemove"] = function(e) {
+			handleMove(e.pageX, e.type);
+		};
+		handlers["touchmove"] = function (e) {
+			e.preventDefault();
+			var touch = e.originalEvent.touches[0] || e.originalEvent.changedTouches[0];
+			handleMove(touch.pageX, e.type);
+		};
 
-		$(config.Selector).on("mousedown", function(data) {
-			$(this).on("mousemove", mouseMove);
-		});
-		$(config.Selector).on("mouseup", function(data) {
-			clear();
+		$(config.Selector).on({
+			touchstart: function(e) {
+				e.preventDefault();
+				$(this).on("touchmove", handlers["touchmove"]);
+			}, mousedown: function() {
+				$(this).on("mousemove", handlers["mousemove"]);
+			}, touchend: function(e) {
+				e.preventDefault();
+				clear("touchmove");
+			}, mouseup: function() {
+				clear("mousemove");
+			}
 		});
 	};
 
